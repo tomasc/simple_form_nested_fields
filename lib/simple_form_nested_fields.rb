7101f6641a1849e1ab56335f4f8f9012d4cdaa26
asgerb <a@asgerbehnckejacobsen.dk>
2018-10-13 09:28:46 +0200

    Add railtie

--- lib/simple_form_nested_fields.rb
+++ lib/simple_form_nested_fields.rb
@@ -7,6 +7,7 @@ require 'simple_form_nested_fields/version'
 
 require 'simple_form_nested_fields/action_view_extension'
 require 'simple_form_nested_fields/nested_fields_builder'
+require 'simple_form_nested_fields/railtie' if defined?(Rails)
 
 I18n.load_path += Dir.glob(File.join( File.dirname(__FILE__), 'config', 'locales', '*.yml' ))
 
