aeed9c73fcacbf08dbc9b9e7167a7373c7fea79d
asgerb <a@asgerbehnckejacobsen.dk>
2019-10-11 21:51:23 +0200

    Remove asset_path method

--- lib/simple_form_nested_fields.rb
+++ lib/simple_form_nested_fields.rb
@@ -10,9 +10,3 @@ require 'simple_form_nested_fields/action_view_extension'
 require 'simple_form_nested_fields/nested_fields_builder'
 
 I18n.load_path += Dir.glob(File.join(File.dirname(__FILE__), 'config', 'locales', '*.yml'))
-
-module SimpleFormNestedFields
-  def self.asset_path
-    File.expand_path('assets/javascripts', __dir__)
-  end
-end
