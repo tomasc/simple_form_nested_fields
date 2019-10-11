89b6cf8786b94c0eadec9317ad8a473e2799a0f5
asgerb <a@asgerbehnckejacobsen.dk>
2019-10-11 21:50:51 +0200

    Remove sprockets railtie

--- lib/simple_form_nested_fields.rb
+++ lib/simple_form_nested_fields.rb
@@ -8,7 +8,6 @@ require 'simple_form_nested_fields/bem'
 
 require 'simple_form_nested_fields/action_view_extension'
 require 'simple_form_nested_fields/nested_fields_builder'
-require 'simple_form_nested_fields/railtie' if defined?(Rails)
 
 I18n.load_path += Dir.glob(File.join(File.dirname(__FILE__), 'config', 'locales', '*.yml'))
 
--- lib/simple_form_nested_fields/railtie.rb
+++
@@ -1,9 +0,0 @@
-require 'rails'
-
-module SimpleFormNestedFields
-  class Railtie < Rails::Railtie
-    initializer 'simple_form_dependent_fields.assets' do |app|
-      app.config.assets.paths << SimpleFormNestedFields.asset_path
-    end
-  end
-end
